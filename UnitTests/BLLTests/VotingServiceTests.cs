f2a85c795f81ca7a3b79f6bdb81c5a6d9ca533ef
Chernikov Nikita <14nik20@gmail.com>
2021-11-21 22:59:58 +0200

    Added BLL Test sample

---
+++ UnitTests/BLLTests/VotingServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using DAL;
+using DAL.Entities;
+using BLL;
+using BLL.Models;
+using BLL.Services;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using Moq;
+
+namespace UnitTests.BLLTests
+{
+    public class VotingServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
