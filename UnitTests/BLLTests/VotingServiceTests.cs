1dc80ac449f7639b9b6a3f61922300dd69456579
Chernikov Nikita <14nik20@gmail.com>
2021-11-23 12:41:31 +0200

    Optimized tests' data

--- UnitTests/BLLTests/VotingServiceTests.cs
+++ UnitTests/BLLTests/VotingServiceTests.cs
@@ -27,23 +27,21 @@ namespace UnitTests.BLLTests
         [Test]
         public void GetAll_ReturnsRightEnumerable()
         {
-            using (var context = new ApplicationContext(UnitTestHelper.GetUnitTestDbOptions()))
+            using var context = new ApplicationContext(UnitTestHelper.GetUnitTestDbOptions());
+            // Arrange
+            var expected = context.Votings.Include(v => v.Votes).ToArray();
+            var service = new VotingService(context, _mapper);
+
+            // Act
+            var actual = service.GetAll().ToArray();
+
+            // Assert
+            Assert.AreEqual(expected.Length, actual.Length, "The count of elements received is not right");
+            for (int i = 0; i < expected.Length; i++)
             {
-                // Arrange
-                var expected = context.Votings.Include(v => v.Votes).ToArray();
-                var service = new VotingService(context, _mapper);
-
-                // Act
-                var actual = service.GetAll().ToArray();
-
-                // Assert
-                Assert.AreEqual(expected.Length, actual.Length, "The count of elements received is not right");
-                for (int i = 0; i < expected.Length; i++)
-                {
-                    Assert.AreEqual(expected[i].Id, actual[i].Id, "Elements' ids are not equal");
-                    Assert.AreEqual(expected[i].Name, actual[i].Name, "Elements' names are not equal");
-                    Assert.AreEqual(expected[i].Votes.Count, actual[i].Votes.Count, "Elements' votes counts are not equal");
-                }
+                Assert.AreEqual(expected[i].Id, actual[i].Id, "Elements' ids are not equal");
+                Assert.AreEqual(expected[i].Name, actual[i].Name, "Elements' names are not equal");
+                Assert.AreEqual(expected[i].Votes.Count, actual[i].Votes.Count, "Elements' votes counts are not equal");
             }
         }
 
@@ -52,20 +50,18 @@ namespace UnitTests.BLLTests
         [TestCase(2)]
         public async Task GetByIdAsync_ReturnsRightElement(int id)
         {
-            using (var context = new ApplicationContext(UnitTestHelper.GetUnitTestDbOptions()))
-            {
-                // Arrange
-                var expected = await context.Votings.Include(v => v.Votes).SingleOrDefaultAsync(v => v.Id == id);
-                var service = new VotingService(context, _mapper);
+            using var context = new ApplicationContext(UnitTestHelper.GetUnitTestDbOptions());
+            // Arrange
+            var expected = await context.Votings.Include(v => v.Votes).SingleOrDefaultAsync(v => v.Id == id);
+            var service = new VotingService(context, _mapper);
 
-                // Act
-                var actual = await service.GetByIdAsync(id);
+            // Act
+            var actual = await service.GetByIdAsync(id);
 
-                // Assert
-                Assert.AreEqual(expected.Id, actual.Id, "Elements' ids are not equal");
-                Assert.AreEqual(expected.Name, actual.Name, "Elements' names are not equal");
-                Assert.AreEqual(expected.Votes.Count, actual.Votes.Count, "Elements' votes counts are not equal");
-            }
+            // Assert
+            Assert.AreEqual(expected.Id, actual.Id, "Elements' ids are not equal");
+            Assert.AreEqual(expected.Name, actual.Name, "Elements' names are not equal");
+            Assert.AreEqual(expected.Votes.Count, actual.Votes.Count, "Elements' votes counts are not equal");
         }
     }
 }
