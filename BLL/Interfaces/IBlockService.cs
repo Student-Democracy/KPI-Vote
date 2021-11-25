083fe55e02bba6c8192704ef4340c9e08d0934e9
Viktor <zozulyuk22@gmail.com>
2021-11-25 23:10:57 +0200

    Added BlockModel, IBlockService and BlockService

---
+++ BLL/Interfaces/IBlockService.cs
@@ -0,0 +1,19 @@
+﻿using BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    interface IBlockService : ICRUD<BlockModel>
+    {
+        IEnumerable<BlockModel> GetSortedActiveBlocks();  //active blocks mean that a ban is still continues
+
+        Task<BlockModel> GetByUserIdAsync(string userId);
+
+        IEnumerable<BlockModel> GetSortedByAdminId(string adminId);    
+
+    }
+}
