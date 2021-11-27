94a0c916bb82b5dfaccb142dca0ec06864599fb4
DanyaCt <sebtec1212@gmail.com>
2021-11-27 15:36:05 +0200

    added GroupModel, GroupService, IGroupService

---
+++ BLL/Models/GroupModel.cs
@@ -0,0 +1,21 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace BLL.Models
+{
+    public class GroupModel
+    {
+        public int Id { get; set; }
+
+        public short Number { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public int FlowId { get; set; }
+
+        public ICollection<User> Users { get; set; }
+
+        public ICollection<Voting> Votings { get; set; }
+    }
+}
