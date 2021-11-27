998275b7db44fa9fb9b0ed0832bb68833b644991
Artem <sir.stankov@yandex.ua>
2021-11-27 15:48:00 +0200

    added Flow model, interface, service

---
+++ BLL/Models/FlowModel.cs
@@ -0,0 +1,25 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace BLL.Models
+{
+    public class FlowModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Postfix { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+
+        public int FacultyId { get; set; }
+
+
+        public ICollection<Group> Groups { get; set; }
+
+        public ICollection<Voting> Votings { get; set; }
+    }
+}
