644ee10d95450ccda2d0a8e3f61f9eac57878ea0
Artem <sir.stankov@yandex.ua>
2021-11-27 15:54:44 +0200

    added faculty interface, model, service

---
+++ BLL/Models/FacultyModel.cs
@@ -0,0 +1,25 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace BLL.Models
+{
+    public class FacultyModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Postfix { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+
+        public int FacultyId { get; set; }
+
+
+        public ICollection<Group> Groups { get; set; }
+
+        public ICollection<Voting> Votings { get; set; }
+    }
+}
