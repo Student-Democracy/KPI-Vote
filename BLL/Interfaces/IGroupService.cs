fbb6f75b35514d41c05b50eb7cc52ff2be30c877
DanyaCt <sebtec1212@gmail.com>
2021-11-27 16:46:14 +0200

    added bad uniTest for GroupService

--- BLL/Interfaces/IGroupService.cs
+++ BLL/Interfaces/IGroupService.cs
@@ -1,14 +1,11 @@
 ﻿using BLL.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace BLL.Interfaces
 {
     interface IGroupService : ICRUD<GroupModel>
     {
-
+       
     }
 }
---
+++ UnitTests/BLLTests/GroupServiceTests.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using BLL.Services;
+using DAL;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnitTests.BLLTests
+{
+    public class GroupServiceTests
+    {
+        private IMapper _mapper;
+
+        [SetUp]
+        public void Setup()
+        {
+            _mapper = UnitTestHelper.CreateMapperProfile();
+        }
+
+        [Test]
+        public void GetAll_ReturnsRightEnumerable()
+        {
+            using var context = new ApplicationContext(UnitTestHelper.GetUnitTestDbOptions());
+            // Arrange
+            var expected = context.Groups.ToArray();
+            var service = new GroupService(context, _mapper);
+
+            // Act
+            var actual = service.GetAll().ToArray();
+
+            // Assert
+            Assert.AreEqual(expected.Length, actual.Length, "The count of elements received is not right");
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i].Id, actual[i].Id, "Elements' ids are not equal");
+                Assert.AreEqual(expected[i].Number, actual[i].Number, "Elements' numbers are not equal");
+            }
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(2)]
+        public async Task GetByIdAsync_ReturnsRightElement(int id)
+        {
+            using var context = new ApplicationContext(UnitTestHelper.GetUnitTestDbOptions());
+            // Arrange
+            var expected = await context.Groups.SingleOrDefaultAsync(v => v.Id == id);
+            var service = new GroupService(context, _mapper);
+
+            // Act
+            var actual = await service.GetByIdAsync(id);
+
+            // Assert
+            Assert.AreEqual(expected.Id, actual.Id, "Elements' ids are not equal");
+            Assert.AreEqual(expected.Number, actual.Number, "Elements' Number are not equal");
+        }
+    }
+}
