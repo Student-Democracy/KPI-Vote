50394fa891de320e857056f2df471c7b228eb225
Viktor <zozulyuk22@gmail.com>
2021-11-28 20:48:12 +0200

    Updated the BlockServiceTests

--- BLL/Services/BlockService.cs
+++ BLL/Services/BlockService.cs
@@ -19,6 +19,7 @@ namespace BLL.Services
         private readonly IMapper _mapper;
 
         private const short _minHammerLength = 10;
+        private const short _maxHammerLength = 2000;
 
         public BlockService(ApplicationContext context, IMapper mapper)
         {
@@ -28,6 +29,8 @@ namespace BLL.Services
 
         public async Task AddAsync(BlockModel model)
         {
+            if(model is null)
+                throw new ArgumentNullException(nameof(model), "Model cannot be null");
             model.DateFrom = DateTime.Now;            
             if (model.DateTo < model.DateFrom)
                 throw new ArgumentException($"Ban date to the end cannot be less than date from the beginning",
@@ -37,11 +40,14 @@ namespace BLL.Services
             if (model.Hammer.Length < _minHammerLength)
                 throw new ArgumentException($"Ban hammer should contain at least {_minHammerLength} characters",
                     model.Hammer);
+            if (model.Hammer.Length > _maxHammerLength)
+                throw new ArgumentException($"Ban hammer should contain at not more {_maxHammerLength} characters",
+                    model.Hammer);
             if (string.IsNullOrEmpty(model.UserId))
                 throw new ArgumentNullException(nameof(model), "Model's user id cannot be null or empty");
             if (await _context.Users.FindAsync(model.UserId) is null)
                 throw new ArgumentNullException(nameof(model), "User with such an id was not found");
-            if (await _context.Bans.FindAsync(model.Id) != null)
+            if (!(await _context.Bans.SingleOrDefaultAsync(p => p.UserId == model.UserId) is null))
                 throw new ArgumentException("This user is already blocked", nameof(model)); 
             if (string.IsNullOrEmpty(model.AdminId))
                 throw new ArgumentNullException(nameof(model), "Model's admin id cannot be null or empty");
@@ -57,7 +63,7 @@ namespace BLL.Services
         {
             var model = await _context.Bans.FindAsync(id);
             if (model is null)
-                throw new ArgumentNullException(nameof(id), "Ban (block) with such an id was not found");
+                throw new InvalidOperationException("Ban (block) with such an id was not found");
             _context.Bans.Remove(model);
             await _context.SaveChangesAsync();
         }
@@ -74,6 +80,8 @@ namespace BLL.Services
 
         public async Task UpdateAsync(BlockModel model)
         {
+            if (model is null)
+                throw new ArgumentNullException(nameof(model), "Model cannot be null");
             if (model.DateTo < model.DateFrom)
                 throw new ArgumentException($"Ban date to the end cannot be less than date from the beginning",
                     nameof(model));
@@ -82,27 +90,42 @@ namespace BLL.Services
             if (model.Hammer.Length < _minHammerLength)
                 throw new ArgumentException($"Ban hammer should contain at least {_minHammerLength} characters",
                     model.Hammer);
+            if (model.Hammer.Length > _maxHammerLength)
+                throw new ArgumentException($"Ban hammer should contain at not more {_maxHammerLength} characters",
+                    model.Hammer);
             if (string.IsNullOrEmpty(model.UserId))
                 throw new ArgumentNullException(nameof(model), "Model's user id cannot be null or empty");
             if (await _context.Users.FindAsync(model.UserId) is null)
                 throw new ArgumentNullException(nameof(model), "User with such an id was not found");
-            if (await _context.Bans.SingleOrDefaultAsync(p => p.UserId == model.UserId) != null)
+            if (!(await _context.Bans.SingleOrDefaultAsync(p => p.UserId == model.UserId) is null))
                 throw new ArgumentException("This user is already blocked", nameof(model));
             if (string.IsNullOrEmpty(model.AdminId))
                 throw new ArgumentNullException(nameof(model), "Model's admin id cannot be null or empty");
             if (await _context.Users.FindAsync(model.AdminId) is null)
                 throw new ArgumentNullException(nameof(model), "Admin with such an id was not found");
+            if (model.UserId == model.AdminId)
+                throw new ArgumentException(nameof(model), "Admin cannot block himself/herself");
             _context.Bans.Update(_mapper.Map<Ban>(model));
             await _context.SaveChangesAsync();
         }
 
         public async Task<BlockModel> GetByUserIdAsync(string userId)
         {
+            if (userId is null || userId == "")
+                throw new ArgumentNullException("Model's user id cannot be null or empty");
+            if (await _context.Users.FindAsync(userId) is null)
+                throw new ArgumentNullException("User with such an id was not found");
+
             return _mapper.Map<BlockModel>(await _context.Bans.SingleOrDefaultAsync(p => p.UserId == userId && p.DateTo>=DateTime.Now));
         }
 
         public async Task<IEnumerable<BlockModel>> GetSortedByAdminIdAsync(string adminId)
         {
+            if (adminId is null)
+                throw new ArgumentNullException("Admin id cannot be null or empty");
+            if (await _context.Users.FindAsync(adminId) is null)
+                throw new ArgumentNullException("Admin with such an id was not found");
+
             var blocks = await Task.Run(() => _context.Bans.Where(v => v.AdminId == adminId)
                                                                      .OrderByDescending(v => v.DateFrom));
             return _mapper.Map<IEnumerable<BlockModel>>(blocks);
