75d0bfb85efd76f30ca7d3684a4eec24a1adb4c6
Chernikov Nikita <14nik20@gmail.com>
2021-12-19 00:54:23 +0200

    Added ChangeEmail

--- PL/Controllers/AccountController.cs
+++ PL/Controllers/AccountController.cs
@@ -14,9 +14,13 @@ namespace PL.Controllers
     public class AccountController : Controller
     {
         private readonly SignInManager<User> _signInManager;
-        public AccountController(SignInManager<User> signInManager)
+
+        private readonly UserManager<User> _userManager;
+
+        public AccountController(SignInManager<User> signInManager, UserManager<User> userManager = null)
         {
             _signInManager = signInManager;
+            _userManager = userManager;
         }
 
         [HttpGet]
@@ -57,6 +61,37 @@ namespace PL.Controllers
         {
             await _signInManager.SignOutAsync();
             return RedirectToAction("Login", "Account");
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult ChangeEmail()
+        {
+            return View();
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> ChangeEmail(ChangeEmailViewModel model)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                user.UserName = model.NewEmail;
+                user.NormalizedUserName = model.NewEmail.ToUpperInvariant();
+                user.Email = model.NewEmail;
+                user.NormalizedEmail = user.NormalizedUserName;
+                var result = await _userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                    return RedirectToAction("Index", "Cabinet");
+                else
+                    ModelState.AddModelError("EmailIsTakenError", "Така електронна пошта вже зайнята");
+            }
+            else
+            {
+                ModelState.AddModelError("PasswordError", "Неправильний пароль");
+            }
+            return View();
         }
     }
 }
