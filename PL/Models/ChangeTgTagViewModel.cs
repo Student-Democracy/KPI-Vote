c6013d99294245e2f20aab0d21654016dd86302b
Chernikov Nikita <14nik20@gmail.com>
2022-01-11 15:51:15 +0200

    Redesigned edit buttons, added tgTagChange, fixed bugs

---
+++ PL/Models/ChangeTgTagViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PL.Models
+{
+    public class ChangeTgTagViewModel
+    {
+        [Required]
+        [Display(Name = "TgTag")]
+        public string Tag { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+    }
+}
---
+++ PL/wwwroot/js/validateTgTag.js
@@ -0,0 +1,24 @@
+﻿let tag = "#tgTag";
+
+function checkTgTag(tag) {
+    let value = $(tag).val();
+    let isValid = value.startsWith('@') && value.length >= 6;
+    let warn = "#validateTgTag";
+    if (!isValid) {
+        $(warn).html('Тег повинен починатися з символа @<br>та містити не менш ніж 6 символів, враховуючи @');
+    }
+    else {
+        $(warn).text('');
+    }
+    return isValid;
+}
+
+$(tag).keyup(function () {
+    checkTgTag(this);
+});
+
+let form = '#changeTgTagForm';
+$(form).submit(function (event) {
+    if (!checkTgTag(tag))
+        event.preventDefault();
+});
