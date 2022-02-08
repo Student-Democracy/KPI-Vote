e10890eb5dbd1ed7575c15ac0f7ed5d78f9ea366
Chernikov Nikita <14nik20@gmail.com>
2022-02-08 10:17:02 +0200

    Deleted unnecessary columns in table Bans

---
+++ DAL/Migrations/20220208081340_UpdateBans.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class UpdateBans : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreationDate",
+                table: "Bans");
+
+            migrationBuilder.DropColumn(
+                name: "FlowId",
+                table: "Bans");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreationDate",
+                table: "Bans",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "FlowId",
+                table: "Bans",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
